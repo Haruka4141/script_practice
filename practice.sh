9a9dbb0bf0854f025b0e7f7579c00e54ca842710
howard <howard.chang@aicpc.com.tw>
2021-12-22 15:18:06 +0800

    add bi & if, text edit

--- practice.sh
+++ practice.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash     # the shell name (load .bashrc)
                     # BASH: Bourne Again SHell
 
-echo -e "Hello World!"
+echo "Hello World!"
 # read -p "Please input your first name: " firstname
 # read -p "Please input your last name: " lastname
 # echo -e "\nYour full name is : $firstname $lastname"
 
-read -p "Please input your filename: " fileuser
+# read -p "Please input your filename: " fileuser
 #                   - if fileuser is unset, fileuser="filename"
 #                  :- if fileuser is empty or unset, fileuser="filename"
 filename=${fileuser:-"filename"}
@@ -18,8 +18,20 @@ file1=$filename$date
 # touch "$file1"      # create empty file
 
 #calculate pi
-read -p "The scale number(10~10000): " checking
+# read -p "The scale number(10~10000): " checking
 num=${checking:-"10"}
 time echo "scale=$num; 4*a(1)" | bc -lq     # time: print execute time
+                                            # bc: calculate tool
+# fail...                                            
+# read -p "Please input Y/N : " yn
+# [ "$yn" == "Y" || "$yn" == "y" ] && echo "Yes" && exit 0
+# [ "$yn" == "N" || "$yn" == "n" ] && echo "No" && exit 0
 
-exit 0
+# if [ "$yn" == "Y" ] || [ "$yn" == "y" ]; then
+#     echo "Yes" && exit 0
+# fi
+# if [ "$yn" == "N" ] || [ "$yn" == "n" ]; then
+#     echo "No" && exit 0
+# fi
+echo "I don't know your choice!" && exit 0
+
